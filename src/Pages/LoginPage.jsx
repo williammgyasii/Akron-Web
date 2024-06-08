ad6cc8e1def7508d1fefd6e2a4b867b959803840
William Gyasi <gyasi.wk@gmail.com>
2024-06-08 05:17:09 +0000

    Ongoing

---
+++ src/Pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>This is the Login Page</div>
+  )
+}
+
+export default LoginPage
---
+++ src/Pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RegisterPage() {
+  return (
+    <div>Register Page to Sign In User</div>
+  )
+}
+
+export default RegisterPage
