87004e9842437b800a21a7521eabcca3607c4c1e
William Gyasi <gyasi.wk@gmail.com>
2024-06-14 01:02:47 +0000

    Groupings System

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import RootLayout from "./Routes/RootLayout";
 import LoginPage from "./Pages/LoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import ProtectedRoute from "./Routes/ProtectedRoute";
-import { Dashboard } from "@mui/icons-material";
 import LandingPage from "./Pages/LandingPage";
 import ProfilePage from "./Pages/ProfilePage";
+import DashboardPage from "./Pages/DashboardPage";
 
 function App() {
   const router = createBrowserRouter(
@@ -24,7 +24,7 @@ function App() {
 
         {/* PROTECTED ROUTES */}
         <Route path="home" element={<ProtectedRoute />}>
-          <Route index element={<Dashboard />} />
+          <Route index element={<DashboardPage />} />
           <Route path="profile" element={<ProfilePage />} />
         </Route>
       </Route>
