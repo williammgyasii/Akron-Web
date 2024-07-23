ae3665db4c6b92114d4fe04caff31eee65f60e6e
William Gyasi <gyasi.wk@gmail.com>
2024-07-23 20:51:27 +0000

    Adding Login Logic

--- src/Pages/HomePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HomePage() {
-  return (
-    <div>I am the Homepage of the app</div>
-  )
-}
-
-export default HomePage
--- src/Routes/RootLayout.jsx
+++ src/Routes/RootLayout.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import LoadingView from "../Components/LoadingView";
+import { Container } from "@mui/material";
 
 function RootLayout() {
   const dispatch = useDispatch();
 
-  return false ? <LoadingView /> : <Outlet />;
+  return false ? (
+    <LoadingView />
+  ) : (
+    <Container disableGutters>
+      <Outlet />
+    </Container>
+  );
 }
 
 export default RootLayout;
