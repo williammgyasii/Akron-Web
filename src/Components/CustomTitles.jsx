3b072d488052da1586e9ec7d714d2748faec29ce
William Gyasi <gyasi.wk@gmail.com>
2024-09-04 23:59:46 +0000

    Animated

--- src/Components/CustomTitles.jsx
+++ src/Components/CustomTitles.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Typography, useTheme } from "@mui/material";
-
+import { motion } from "framer-motion";
 //REWRITE OPTIMIZATION
+
+const AnimatedTypography = motion(Typography); // Wrapping the MUI Card with Framer Motion
 
 const CustomTitles = ({
   variant,
@@ -22,7 +24,7 @@ const CustomTitles = ({
     superBold: 900,
   };
   return (
-    <Typography
+    <AnimatedTypography
       variant={variant}
       align={align}
       color={color}
@@ -33,22 +35,33 @@ const CustomTitles = ({
           display: "block",
           fontFamily: "Inter",
           position: "relative",
-          "&::before": {
-            content: '""',
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            background: theme.palette.,
-            zIndex: -1, // Puts the pseudo-element behind the content
-          },
         },
       ]}
       {...props}
     >
+      <motion.span
+        initial={{ opacity: 0.5 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 2,
+          ease: "easeInOut",
+          repeat: Infinity, // Repeats indefinitely
+          repeatType: "reverse", // Reverses the animation direction each time
+        }}
+        style={{
+          position: "absolute",
+          bottom: 5,
+          width: "100px",
+          height: "4px",
+          background: theme.palette.warning.light400,
+          zIndex: -1, // Puts the pseudo-element behind the content
+          background: "rgba(0, 0, 255, 0.5)", // Pseudo-element background
+          zIndex: -1, // Puts the pseudo-element behind
+          transition: "background 1.5s ease-in-out",
+        }} // CSS transition for pseudo-element}}
+      ></motion.span>
       {children}
-    </Typography>
+    </AnimatedTypography>
   );
 };
 
